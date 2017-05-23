67cff8d52d2f92f38f6b3770c09bff4985c99826
Kefeng91 <keurfonluu@outlook.com>
2017-05-24 01:01:30 +0200

    Preparing release 1.3.0
    moved gui to a specific subpackage
    cleaning the code
    added seed
    added clipping to edges
    added export functions (with pickle)

---
+++ example/stochopy_viewer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Run StochOPy Viewer to see how popular stochastic algorithm work, and play with
+the tuning parameters on several benchmark functions.
+
+Author: Keurfon Luu <keurfon.luu@mines-paristech.fr>
+License: MIT
+"""
+
+try:
+    from stochopy.gui import main
+except ImportError:
+    import sys
+    sys.path.append("../")
+    from stochopy.gui import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ stochopy/gui/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+StochOPy Viewer allows to play with the parameters of StochOPy solvers on
+popular benchmark functions.
+
+Author: Keurfon Luu <keurfon.luu@mines-paristech.fr>
+License: MIT
+"""
+
+from .gui import StochOGUI, main
+
+__all__ = [ "StochOGUI", "main" ]
