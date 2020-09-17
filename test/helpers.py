da24e5c38f37a17a98de979218a1730b33cfafe9
Keurfon Luu <keurfonluu@outlook.com>
2020-09-17 00:07:08 -0700

    update tests

---
+++ test/helpers.py
@@ -0,0 +1,34 @@
+import numpy
+
+import stochopy
+
+
+def rosenbrock():
+    fun = stochopy.factory.rosenbrock
+    bounds = [[-5.12, 5.12], [-5.12, 5.12]]
+
+    return fun, bounds
+
+
+def optimize(method, options, xref):
+    options.update({
+        "maxiter": 128,
+        "popsize": 8,
+        "seed": 42,
+        "return_all": True,
+    })
+    fun, bounds = rosenbrock()
+    x = stochopy.optimize.minimize(fun, bounds, options=options, method=method)
+
+    assert numpy.allclose(xref, x.x)
+    if options["constraints"] is not None:
+        assert numpy.all(x.xall + 1.0e-15 >= -5.12)
+        assert numpy.all(x.xall - 1.0e-15 <= 5.12)
+
+
+def sample(method, options, xref):
+    options.update({"seed": 42})
+    fun, bounds = rosenbrock()
+    x = stochopy.sample.sample(fun, bounds, options=options, method=method)
+
+    assert numpy.allclose(xref, x.x)
---
+++ test/test_optimize.py
@@ -0,0 +1,135 @@
+import pytest
+
+import helpers
+
+
+@pytest.mark.parametrize(
+    "options, xref",
+    [
+        ({"constraints": None}, [0.29967256, 0.0803311]),
+        ({"xmean0": [-5.0, -5.0], "constraints": None}, [0.99998135, 0.99995618]),
+        ({"constraints": "Penalize"}, [0.18765786, 0.05858025]),
+        ({"xmean0": [-5.0, -5.0], "constraints": "Penalize"}, [0.99998135, 0.99995618]),
+    ],
+)
+def test_cmaes(options, xref):
+    # Serial
+    options.update({
+        "sigma": 0.1,
+        "muperc": 0.5,
+    })
+    helpers.optimize("cmaes", options, xref)
+
+    # Parallel
+    options.update({
+        "workers": 2,
+        "backend": "joblib",
+    })
+    helpers.optimize("cmaes", options, xref)
+
+
+@pytest.mark.parametrize(
+    "options, xref",
+    [
+        ({"inertia": 0.7298, "constraints": None, "updating": "deferred"}, [0.95990315, 0.92082304]),
+        ({"inertia": 0.7298, "constraints": None, "updating": "immediate"}, [0.93258856, 0.86919435]),
+        ({"inertia": 0.91, "constraints": "Shrink", "updating": "deferred"}, [0.73752093, 0.54625484]),
+        ({"inertia": 0.91, "constraints": "Shrink", "updating": "immediate"}, [0.76668308, 0.58381385]),
+    ],
+)
+def test_cpso(options, xref):
+    # Serial
+    options.update({
+        "cognitivity": 1.49618,
+        "sociability": 1.49618,
+        "competitivity": 1.0,
+    })
+    helpers.optimize("cpso", options, xref)
+
+    # Parallel
+    if options["updating"] == "deferred":
+        options.update({
+            "workers": 2,
+            "backend": "joblib",
+        })
+        helpers.optimize("cpso", options, xref)
+
+
+@pytest.mark.parametrize(
+    "options, xref",
+    [
+        ({"strategy": "rand1bin", "constraints": None, "updating": "deferred"}, [0.83228338, 0.68910339]),
+        ({"strategy": "rand2bin", "constraints": None, "updating": "deferred"}, [0.79409325, 0.60743767]),
+        ({"strategy": "best1bin", "constraints": None, "updating": "deferred"}, [1.00025932, 1.00051521]),
+        ({"strategy": "best2bin", "constraints": None, "updating": "deferred"}, [1.00515037, 1.01055037]),
+        ({"strategy": "rand1bin", "constraints": None, "updating": "immediate"}, [0.85658185, 0.726094]),
+        ({"strategy": "rand1bin", "constraints": "Random", "updating": "deferred"}, [1.02340815, 1.04590782]),
+        ({"strategy": "rand1bin", "constraints": "Random", "updating": "immediate"}, [0.99438151, 0.9944796]),
+    ],
+)
+def test_de(options, xref):
+    # Serial
+    options.update({
+        "recombination": 0.1,
+        "mutation": 0.5,
+    })
+    helpers.optimize("de", options, xref)
+
+    # Parallel
+    if options["updating"] == "deferred":
+        options.update({
+            "workers": 2,
+            "backend": "joblib",
+        })
+        helpers.optimize("de", options, xref)
+
+
+@pytest.mark.parametrize(
+    "options, xref",
+    [
+        ({"inertia": 0.7298, "constraints": None, "updating": "deferred"}, [0.95990315, 0.92082304]),
+        ({"inertia": 0.7298, "constraints": None, "updating": "immediate"}, [0.95909508, 0.91977272]),
+        ({"inertia": 0.91, "constraints": "Shrink", "updating": "deferred"}, [0.73752093, 0.54625484]),
+        ({"inertia": 0.91, "constraints": "Shrink", "updating": "immediate"}, [0.76668308, 0.58381385]),
+    ],
+)
+def test_pso(options, xref):
+    # Serial
+    options.update({
+        "cognitivity": 1.49618,
+        "sociability": 1.49618,
+    })
+    helpers.optimize("pso", options, xref)
+
+    # Parallel
+    if options["updating"] == "deferred":
+        options.update({
+            "workers": 2,
+            "backend": "joblib",
+        })
+        helpers.optimize("pso", options, xref)
+
+
+@pytest.mark.parametrize(
+    "options, xref",
+    [
+        ({"constraints": None}, [0.90013445, 0.85037782]),
+        ({"xmean0": [-5.0, -5.0], "constraints": None}, [0.84059993, 0.69998341]),
+        ({"constraints": "Penalize"}, [0.90013445, 0.85037782]),
+        ({"xmean0": [-5.0, -5.0], "constraints": "Penalize"}, [0.82405114, 0.61993136]),
+    ],
+)
+def test_vdcma(options, xref):
+    # Serial
+    options.update({
+        "sigma": 0.1,
+        "muperc": 0.5,
+    })
+    helpers.optimize("vdcma", options, xref)
+
+    # Parallel
+    options.update({
+        "workers": 2,
+        "backend": "joblib",
+    })
+    helpers.optimize("vdcma", options, xref)
